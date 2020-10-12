7808541a9154462e9249f2b8fd866dce7645ef4e
Dev Patel <46830802+dev4848@users.noreply.github.com>
2020-10-12 17:01:36 -0400

    Added kotlin class files for xml pages

---
+++ app/src/main/java/com/example/diningdecider/FavoritePage.kt
@@ -0,0 +1,11 @@
+package com.example.diningdecider
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FavoritePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.favorites)
+    }
+}
