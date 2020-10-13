84799651488185e3af6fb68a944e2cc9a9c86254
Anagha Late <anagha20@hotmail.com>
2020-10-13 14:56:28 -0400

    back button added !

--- app/src/main/java/com/example/diningdecider/random_page.kt
+++ app/src/main/java/com/example/diningdecider/random_page.kt
@@ -3,12 +3,14 @@ package com.example.diningdecider
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class random_page : AppCompatActivity() {
 
     //Random Page Buttons
-    private lateinit var dineButton: Button
+    //private lateinit var dineButton: Button
     private lateinit var newButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,17 +19,21 @@ class random_page : AppCompatActivity() {
 
 
         //Random Screen Buttons
-        dineButton = findViewById(R.id.button3)
+        //dineButton = findViewById(R.id.button3)
         newButton = findViewById(R.id.button2)
 
-        //Random Screen - What to set the dine button to? and edit new button
-        dineButton.setOnClickListener {
-            intent = Intent(this, WinnerPage::class.java)
-            startActivity(intent)
-        }
+//        //Random Screen - What to set the dine button to? and edit new button
+//        dineButton.setOnClickListener {
+//            intent = Intent(this, WinnerPage::class.java)
+//            startActivity(intent)
+//        }
+
         newButton.setOnClickListener {
             intent = Intent(this, random_page::class.java)
             startActivity(intent)
         }
     }
+
+
+
 }
